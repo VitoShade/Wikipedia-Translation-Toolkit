3606a3ac08754ab239884074bfb52fd1622ef9cf
nik-konto <nicolo.contini@gmail.com>
2021-01-13 20:06:43 +0100

    Funziona ma crea un sacco di file

--- src/main/scala/prepareData.scala
+++ src/main/scala/prepareData.scala
@@ -24,52 +24,88 @@ object prepareData extends App {
     // For implicit conversions like converting RDDs to DataFrames
     import sparkSession.implicits._
 
-    val inputFile = "C:\\Users\\nik_9\\Desktop\\prova\\indice10.txt"
-    val outputFile = "C:\\Users\\nik_9\\Desktop\\prova\\result"
+    val inputFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\indici"
+    val tempFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\tempResult"
+    val outputFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\result"
 
-    val input:org.apache.spark.rdd.RDD[String] = sparkContext.textFile(inputFile)
+    val inputFolder = new File(inputFolderName)
 
-    val result = input.map(line => {
+    val files: Array[String] = inputFolder.listFiles.filter(file => file.isFile && (file.toString.takeRight(4) == ".txt")).map(file => file.toString)
 
-      var tuple1 = APILangLinks.callAPI(line, "en", "it")
-      val num_traduzioni = tuple1._1
-      val id_pagina_italiana = tuple1._2
-      //println(tuple1)
+    var outputFolders: Array[String] = Array[String]()
 
-      var tuple2 = APIPageView.callAPI(line, "en")
-      val num_visualiz_anno = tuple2._1
-      val num_visualiz_mesi = tuple2._2
-      //println(line + "   " + tuple2)
-      //println(num_visualiz_anno)
+    //files.foreach(println)
+    files.foreach(inputFileName => {
+      val input: org.apache.spark.rdd.RDD[String] = sparkContext.textFile(inputFileName)
 
-      var tuple3 = APIRedirect.callAPI(line, "en")
-      val byte_dim_page = tuple3._1
-      val id_redirect = tuple3._2
-      //println(result3)
+      val result = input.map(line => {
 
-      Entry(line, num_traduzioni, id_pagina_italiana, num_visualiz_anno, num_visualiz_mesi, byte_dim_page, id_redirect)
-    }).persist()
+        //println(line)
 
-    //println(result)
+        var tuple1 = APILangLinks.callAPI(line, "en", "it")
+        val num_traduzioni = tuple1._1
+        val id_pagina_italiana = tuple1._2
+        //println(tuple1)
 
-    val resultDataFrame = result.toDF("id", "num_traduzioni", "id_pagina_italiana", "num_visualiz_anno", "num_visualiz_mesi", "byte_dim_page", "id_redirect")
+        var tuple2 = APIPageView.callAPI(line, "en")
+        val num_visualiz_anno = tuple2._1
+        val num_visualiz_mesi = tuple2._2
+        //println(line + "   " + tuple2)
+        //println(num_visualiz_anno)
 
-    //resultDataFrame.show(false)
+        var tuple3 = APIRedirect.callAPI(line, "en")
+        val byte_dim_page = tuple3._1
+        val id_redirect = tuple3._2
+        //println(result3)
 
-    FileUtils.deleteDirectory(new File(outputFile))
-    resultDataFrame.write.parquet(outputFile)
+        Entry(line, num_traduzioni, id_pagina_italiana, num_visualiz_anno, num_visualiz_mesi, byte_dim_page, id_redirect)
+      }).persist
 
-    val folder = new File(outputFile)
+      //println(result)
 
-    val files: Array[String] = folder.listFiles.filter(file => file.isFile && (file.toString.takeRight(15) == ".snappy.parquet")).map(file => file toString)
+      val resultDataFrame = result.toDF("id", "num_traduzioni", "id_pagina_italiana", "num_visualiz_anno", "num_visualiz_mesi", "byte_dim_page", "id_redirect")
 
-    val dataFrameFromFiles = files map (n => sparkSession.read.parquet(n))
+      //resultDataFrame.show(false)
 
-    dataFrameFromFiles.foreach(_.show(false))
+      val outputName = inputFileName.drop(36).dropRight(4)
+      val outputFolder = tempFolderName + "\\" + outputName
+
+      outputFolders = outputFolders :+ outputFolder
+
+      //println(outputFolder)
+
+      FileUtils.deleteDirectory(new File(outputFolder))
+      resultDataFrame.write.parquet(outputFolder)
+
+    })
+
+    //outputFolders.foreach(println)
+
+    var allFiles: Array[String] = Array[String]()
+
+    outputFolders.foreach(tempFolder => {
+        val folder = new File(tempFolder)
+
+        val files: Array[String] = folder.listFiles.filter(file => file.isFile && (file.toString.takeRight(15) == ".snappy.parquet")).map(file => file toString)
+
+        allFiles = allFiles ++ files
+
+      }
+    )
+
+    //allFiles.foreach(println)
+
+    val dataFrameFromFiles = allFiles map (n => sparkSession.read.parquet(n))
+
+    //dataFrameFromFiles.foreach(_.show(false))
 
     val res2 = dataFrameFromFiles.reduce(_ union _)
 
     res2.show(false)
+
+    FileUtils.deleteDirectory(new File(outputFolderName))
+    res2.write.parquet(outputFolderName)
+
 
     //ferma anche lo sparkContext
     sparkSession.stop()
