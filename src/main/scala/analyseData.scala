887efe80ca8e5393f8073e49cf3f0acd61f4aab8
nik-konto <nicolo.contini@gmail.com>
2021-01-20 16:56:25 +0100

    Summarizer completo per en.wikipedia

--- src/main/scala/analyseData.scala
+++ src/main/scala/analyseData.scala
@@ -1,15 +1,16 @@
 
 import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions._
 import API._
 import Utilities._
 import org.apache.commons.io.FileUtils
 import org.apache.spark.sql.functions.{concat, desc, length}
-
 import java.io._
 import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 import org.apache.spark.ml.stat.Summarizer
 import org.apache.spark.ml.linalg._
+
 
 import scala.collection.mutable
 
@@ -21,7 +22,7 @@ case class EntryClean(id: String,
 object analyseData extends App {
   override def main(args: Array[String]) {
 
-    val sparkSession = SparkSession.builder().master("local[25]").appName("prepareData").getOrCreate()
+    val sparkSession = SparkSession.builder().master("local[16]").appName("analyseData").getOrCreate()
     val sparkContext = sparkSession.sparkContext
 
     sparkContext.setLogLevel("WARN")
@@ -39,23 +40,28 @@ object analyseData extends App {
     val errorFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\result\\error"*/
     val folderSeparator = "\\"
 
+    //cartella parquet inglesi
     val inputFolders = Array(inputFolderName + folderSeparator + "en")
 
     val allInputFolders = DataFrameUtility.collectParquetFilesFromFolders(inputFolders)
 
     val dataFrameFilesSrc = allInputFolders map (tempFile => sparkSession.read.parquet(tempFile))
 
+    //merge dei parquet in un file unico
     val dataFrameSrc = dataFrameFilesSrc.reduce(_ union _)
 
     //dataFrameSrc.show(false)
 
+    //selezione delle righe che sono redirect
     val dataFrameRedirect = dataFrameSrc.
       filter("id_redirect != ''").
       select("num_visualiz_anno","num_visualiz_mesi","id_redirect")
 
     //dataFrameRedirect.show(false)
 
-    val nuovo = dataFrameRedirect.map(row => {
+    //cast degli array del dataFrame da Int a Double
+    val dataFrameRedirectDouble = dataFrameRedirect.map(row => {
+
       val anniInt = row.getAs[mutable.WrappedArray[Int]]("num_visualiz_anno").toArray
       val anniDouble = anniInt map (_.toDouble)
 
@@ -69,15 +75,19 @@ object analyseData extends App {
       )
     })
 
-    nuovo.show(false)
+    //creazione del dataFrame a partire dal dataSet
+    val newDataFrameRedirect = dataFrameRedirectDouble.toDF("id_redirect", "num_visualiz_anno", "num_visualiz_mesi")
 
-    val prova = nuovo.groupBy($"id").agg(
-      Summarizer.sum($"numVisualizzazioniAnno").alias("numVisAnno"),
-      Summarizer.sum($"numVisualizzazioniMesi").alias("numVisMesi")
-    ).toDF("id_redirect","num_visualiz_anno","num_visualiz_mesi")
+    newDataFrameRedirect.orderBy(asc("id_redirect")).filter("id_redirect == 'American_Indian_boarding_schools'").show(false)
 
-    prova.filter("id_redirect == 'American_Indian_boarding_schools'").show(false)
+    //somma degli array per righe con lo stesso id_redirect
+    val summarizedDataFrameRedirect = newDataFrameRedirect.groupBy($"id_redirect").agg(
+      Summarizer.sum($"num_visualiz_anno"),
+      Summarizer.sum($"num_visualiz_mesi")
+    )
 
+    //prova.filter("id_redirect == 'American_Indian_boarding_schools'").show(false)List_of_Native_American_artists
+    summarizedDataFrameRedirect.filter("id_redirect == 'American_Indian_boarding_schools'").show(false)
 
 
 
