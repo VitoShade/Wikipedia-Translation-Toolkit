9c0dcda42459e93259f82cb57bac7058dac544c7
nik-konto <nicolo.contini@gmail.com>
2021-01-20 16:18:01 +0100

    Summarizer funzionante

--- src/main/scala/analyseData.scala
+++ src/main/scala/analyseData.scala
@@ -4,10 +4,19 @@ import API._
 import Utilities._
 import org.apache.commons.io.FileUtils
 import org.apache.spark.sql.functions.{concat, desc, length}
+
 import java.io._
 import java.net.URLDecoder
 import java.nio.charset.StandardCharsets
 import org.apache.spark.ml.stat.Summarizer
+import org.apache.spark.ml.linalg._
+
+import scala.collection.mutable
+
+case class EntryClean(id: String,
+                      numVisualizzazioniAnno: Vector,
+                      numVisualizzazioniMesi: Vector
+                    )
 
 object analyseData extends App {
   override def main(args: Array[String]) {
@@ -22,8 +31,8 @@ object analyseData extends App {
     // For implicit conversions like converting RDDs to DataFrames
     import sparkSession.implicits._
 
-    //val inputFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\outputProcessati\\File1-3"
-    val inputFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\result"
+    val inputFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\outputProcessati\\File1-3"
+    //val inputFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\result"
 
     /*val tempFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\tempResult"
     val outputFolderName = "C:\\Users\\nik_9\\Desktop\\prova\\result"
@@ -32,46 +41,44 @@ object analyseData extends App {
 
     val inputFolders = Array(inputFolderName + folderSeparator + "en")
 
-    /*val allInputFolders = DataFrameUtility.collectParquetFilesFromFolders(inputFolders)
+    val allInputFolders = DataFrameUtility.collectParquetFilesFromFolders(inputFolders)
 
     val dataFrameFilesSrc = allInputFolders map (tempFile => sparkSession.read.parquet(tempFile))
 
-    val dataFrameSrc = dataFrameFilesSrc.reduce(_ union _)*/
+    val dataFrameSrc = dataFrameFilesSrc.reduce(_ union _)
 
     //dataFrameSrc.show(false)
 
-    //val inputRDD = dataFrameSrc.rdd
-
-    /*val dataFrameRedirect = dataFrameSrc.
+    val dataFrameRedirect = dataFrameSrc.
       filter("id_redirect != ''").
-      //select("num_visualiz_anno","num_visualiz_mesi","id_redirect")
-      select("num_visualiz_anno","id_redirect")
+      select("num_visualiz_anno","num_visualiz_mesi","id_redirect")
 
-    dataFrameRedirect.show(false)
+    //dataFrameRedirect.show(false)
 
-    val prova = dataFrameRedirect.groupBy($"id_redirect").agg(Summarizer.sum($"num_visualiz_anno").alias("num_visualiz_anno"))
+    val nuovo = dataFrameRedirect.map(row => {
+      val anniInt = row.getAs[mutable.WrappedArray[Int]]("num_visualiz_anno").toArray
+      val anniDouble = anniInt map (_.toDouble)
 
-    prova.show()*/
+      val mesiInt = row.getAs[mutable.WrappedArray[Int]]("num_visualiz_mesi").toArray
+      val mesiDouble = mesiInt map (_.toDouble)
 
-    val df = sparkSession.createDataFrame(Seq(
-      (1, org.apache.spark.ml.linalg.Vectors.dense(0,0,5)),
-      (1, org.apache.spark.ml.linalg.Vectors.dense(4,0,1)),
-      (1, org.apache.spark.ml.linalg.Vectors.dense(1,2,1)),
-      (2, org.apache.spark.ml.linalg.Vectors.dense(7,5,0)),
-      (2, org.apache.spark.ml.linalg.Vectors.dense(3,3,4)),
-      (3, org.apache.spark.ml.linalg.Vectors.dense(0,8,1)),
-      (3, org.apache.spark.ml.linalg.Vectors.dense(0,0,1)),
-      (3, org.apache.spark.ml.linalg.Vectors.dense(7,7,7)))
-    ).toDF("id", "vec")
+      EntryClean(
+        row.getAs("id_redirect"),
+        org.apache.spark.ml.linalg.Vectors.dense(anniDouble),
+        org.apache.spark.ml.linalg.Vectors.dense(mesiDouble)
+      )
+    })
 
-    df.show(false)
+    nuovo.show(false)
 
-    println(df)
+    val prova = nuovo.groupBy($"id").agg(
+      Summarizer.sum($"numVisualizzazioniAnno").alias("numVisAnno"),
+      Summarizer.sum($"numVisualizzazioniMesi").alias("numVisMesi")
+    ).toDF("id_redirect","num_visualiz_anno","num_visualiz_mesi")
 
-    val res = df.groupBy($"id")
-      .agg(Summarizer.sum($"vec").alias("vec"))
+    prova.filter("id_redirect == 'American_Indian_boarding_schools'").show(false)
 
-    res.show(false)
+
 
 
 
